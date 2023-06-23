ca211771d78e12ed5461becbb522bf10ba426e52
vishalchandel-rh <vchandel@rh.com>
2023-06-24 02:09:38 +0530

    small changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -38,7 +38,10 @@ const Contact = () => {
           rows={10}
           placeholder="Message"
         ></textarea>
-        <button type="submit" className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center">
+        <button
+          type="submit"
+          className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center"
+        >
           Let's Collaborate
         </button>
       </form>
